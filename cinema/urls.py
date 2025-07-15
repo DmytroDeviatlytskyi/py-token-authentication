e2565c699c152fce35469af31442858224e03116
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-07-15 20:36:40 +0200

    Token Authentication API

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,14 +10,55 @@ from cinema.views import (
     OrderViewSet,
 )
 
-router = routers.DefaultRouter()
-router.register("genres", GenreViewSet)
-router.register("actors", ActorViewSet)
-router.register("cinema_halls", CinemaHallViewSet)
-router.register("movies", MovieViewSet)
-router.register("movie_sessions", MovieSessionViewSet)
-router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+genre_list = GenreViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+actor_list = ActorViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+movie_list = MovieViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+movie_detail = MovieViewSet.as_view(actions={
+    "get": "retrieve",
+})
+movie_session_list = MovieSessionViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+movie_session_detail = MovieSessionViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+order_list = OrderViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+
+urlpatterns = [
+    path("genres/", genre_list, name="genre-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinemahall-list"),
+    path("actors/", actor_list, name="actor-list"),
+    path("movies/", movie_list, name="movie-list"),
+    path("movies/<int:pk>", movie_detail, name="movie-detail"),
+    path("movie_sessions/", movie_session_list, name="moviesession-list"),
+    path(
+        "movie_sessions/<int:pk>",
+        movie_session_detail,
+        name="moviesession-detail"
+    ),
+    path("orders/", order_list, name="order-list"),
+
+]
 
 app_name = "cinema"
